5f3af527ad0b72e751805d51ba1ca3e2896d926a
Sean Taylor <silverwolf4350@gmail.com>
2020-07-23 17:04:43 +0000

    Testing requirements.

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -18,7 +18,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	if (tree->right != NULL)
 		counterR = balance(tree->right, 1, 1);
 
-	if (counterL - counterR == 0 && binary_tree_is_full(tree))
+	if (counterL - counterR == 0)
 		return (1);
 	else
 		return (0);
