dbfc05602100f81b3615934b55e17bbbf91bfefe
Jilroge7 <1672@holbertonschool.com>
2020-07-23 15:07:36 +0000

    edit to add edge case

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,6 +10,9 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *tempNode;
 
+	if (parent == NULL)
+		return (NULL);
+
 	tempNode = binary_tree_node(parent, value);
 	if (tempNode == NULL)
 		return (NULL);
