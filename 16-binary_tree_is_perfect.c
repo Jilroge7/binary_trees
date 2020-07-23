3696a99e0fefc6079a901c4a3de19c9066813a31
Sean Taylor <silverwolf4350@gmail.com>
2020-07-23 18:27:36 +0000

    Fixed definition of perfect.

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -7,41 +7,34 @@
 */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	int counterL = 0, counterR = 0;
+	size_t height;
 
 	if (tree == NULL)
 		return (0);
 
-	if (tree->left == NULL && tree->right == NULL)
-		return (0);
+	height = recursive_height(tree, 0, 0);
 
-	if (tree->left != NULL)
-		counterL = balance(tree->left, 1, 1);
-
-	if (tree->right != NULL)
-		counterR = balance(tree->right, 1, 1);
-
-	if (counterL - counterR == 0 && binary_tree_is_full(tree))
+	if (height * height == binary_tree_leaves(tree))
 		return (1);
-	else
-		return (0);
+
+	return (0);
 }
 
 /**
-* balance - function
+* recursive_height - function
 * @tree: ptr to root of tree to traverse through
 * @counter: variable to store traverseal steps
 * @max: variable to store recursive_height
 * Description: gets height of tree
 * Return: height of tree
 */
-int balance(const binary_tree_t *tree, int counter, int max)
+size_t recursive_height(const binary_tree_t *tree, size_t counter, size_t max)
 {
 	if (tree->left != NULL)
-		max = balance(tree->left, counter + 1, max);
+		max = recursive_height(tree->left, counter + 1, max);
 
 	if (tree->right != NULL)
-		max = balance(tree->right, counter + 1, max);
+		max = recursive_height(tree->right, counter + 1, max);
 
 	if (max < counter)
 		max = counter;
@@ -49,28 +42,27 @@ int balance(const binary_tree_t *tree, int counter, int max)
 }
 
 /**
-* binary_tree_is_full - function
-* @tree: tree to check
-* Description: check if a binary tree is full
-* Return: 1 if full, 0 otherwise
+* binary_tree_leaves - function
+* @tree: tree to count leaves
+* Description: count number of leaves in binary tree
+* Return: number of leaves or 0 if tree is empty
 */
-int binary_tree_is_full(const binary_tree_t *tree)
+size_t binary_tree_leaves(const binary_tree_t *tree)
 {
+	size_t counter = 0;
+
 	if (tree == NULL)
 		return (0);
 
 	if (tree->left != NULL)
-		if (binary_tree_is_full(tree->left) == 0)
-			return (0);
+		counter += binary_tree_leaves(tree->left);
 
 	if (tree->right != NULL)
-		if (binary_tree_is_full(tree->right) == 0)
-			return (0);
+		counter += binary_tree_leaves(tree->right);
 
-	if (tree->left != NULL && tree->right != NULL)
-		return (1);
-	else if (tree->left == NULL && tree->right == NULL)
-		return (1);
+	if (tree->left == NULL && tree->right == NULL)
+		return (counter + 1);
 	else
-		return (0);
+		return (counter);
+
 }
--- 16-main.c
+++ 16-main.c
@@ -33,5 +33,8 @@ int main(void)
     binary_tree_print(root);
     perfect = binary_tree_is_perfect(root);
     printf("Perfect: %d\n", perfect);
+
+    
+
     return (0);
 }
